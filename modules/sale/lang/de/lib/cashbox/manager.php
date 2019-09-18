2e85cbf4a992cb007353b8fc310c427aa5b01567
kotopheiop <kotopheiop@gmail.com>
2019-09-18 16:51:30 +0500

    Init core

---
+++ modules/sale/lang/de/lib/cashbox/manager.php
@@ -0,0 +1,3 @@
+<?
+$MESS["SALE_CASHBOX_NOT_FOUND"] = "Es wurde keine verfügbaren Cash-Register gefunden";
+?>
---
+++ modules/socialnetwork/lang/ru/lib/livefeed/intranetnewuser.php
@@ -0,0 +1,3 @@
+<?
+$MESS["SONET_LIVEFEED_INTRANET_NEW_USER_TITLE"] = "«апись о приеме на работу сотрудника #USER_NAME#";
+?>
